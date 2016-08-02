46d17c85430300e03f15017c61a8419fddf7ae81
chen <38033919@qq.com>
2016-08-02 23:01:20 +0800

    指定权限列表

--- app/src/main/java/com/jgkj/parentscycle/user/UserInfo.java
+++ app/src/main/java/com/jgkj/parentscycle/user/UserInfo.java
@@ -6,6 +6,13 @@ import com.jgkj.parentscycle.bean.LoginInfo;
  * Created by chen on 16/7/24.
  */
 public class UserInfo {
+    //权限ID列表
+    public static final String ADMIN_PERMISSION = "1";   //管理员
+    public static final String USUAL_TEACHER_PERMISSION = "2"; //普通老师
+    public static final String WEB_ADMIN_PERMISSION = "3"; //网站管理员
+    public static final String LOOK_AFTER_TEACHER_PERMISSION = "4"; //保育员老师
+
+
     public static String phoneNumber;
     public static boolean isLogined;
     public static LoginInfo loginInfo = new LoginInfo();
