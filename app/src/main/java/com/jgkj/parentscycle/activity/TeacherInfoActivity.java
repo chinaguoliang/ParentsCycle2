8129bfcd59a53ccc260af8737b9ac3279100c7a9
chen <38033919@qq.com>
2016-08-05 01:14:00 +0800

    增加班级管理对话框

--- app/src/main/java/com/jgkj/parentscycle/activity/TeacherInfoActivity.java
+++ app/src/main/java/com/jgkj/parentscycle/activity/TeacherInfoActivity.java
@@ -1,14 +1,24 @@
 package com.jgkj.parentscycle.activity;
 
-import android.content.Intent;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jgkj.parentscycle.R;
+import com.jgkj.parentscycle.adapter.ModifyClassDialogLvAdapter;
 import com.jgkj.parentscycle.adapter.TeacherInfoAdapter;
+import com.jgkj.parentscycle.bean.MakeClassAddPersonInfo;
+import com.jgkj.parentscycle.utils.ToastUtil;
+import com.jgkj.parentscycle.utils.UtilTools;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +30,7 @@ import butterknife.OnClick;
 /**
  * Created by chen on 16/8/3.
  */
-public class TeacherInfoActivity extends BaseActivity implements View.OnClickListener{
+public class TeacherInfoActivity extends BaseActivity implements View.OnClickListener {
     @Bind(R.id.title_bar_layout_rel)
     View titleBg;
 
@@ -37,13 +47,26 @@ public class TeacherInfoActivity extends BaseActivity implements View.OnClickLis
     ListView contentLv;
 
     TeacherInfoAdapter teacherInfoAdapter;
-
+    Dialog mLeaveSchoolDialog;
+    Dialog mModifyClassDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_info_activity);
         ButterKnife.bind(this);
         initViews();
+        contentLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 12) {
+                    //离开幼儿园
+                    showLeaveSchoolDialog();
+                } else if (position == 11) {
+                    //班级管理
+                    showModifyClassDialog();
+                }
+            }
+        });
     }
 
     private List<String> getContentData() {
@@ -68,7 +91,7 @@ public class TeacherInfoActivity extends BaseActivity implements View.OnClickLis
         titleBg.setBackgroundColor(0x00000000);
         rightTv.setVisibility(View.GONE);
         titleTv.setText("李老师");
-        teacherInfoAdapter = new TeacherInfoAdapter(this,getContentData());
+        teacherInfoAdapter = new TeacherInfoAdapter(this, getContentData());
         contentLv.setAdapter(teacherInfoAdapter);
     }
 
@@ -79,5 +102,88 @@ public class TeacherInfoActivity extends BaseActivity implements View.OnClickLis
         if (v == backIv) {
             finish();
         }
+    }
+
+    private View.OnClickListener changePhotoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.leave_school_dialog_confirm_leave_tv) {
+                mLeaveSchoolDialog.dismiss();
+                ToastUtil.showToast(TeacherInfoActivity.this,"已经离开学校", Toast.LENGTH_SHORT);
+            } else if (v.getId() == R.id.leave_school_dialog_cancel_tv) {
+                mLeaveSchoolDialog.dismiss();
+            } else if (v.getId() == R.id.modify_class_dialog_confirm_btn) {
+                mModifyClassDialog.dismiss();
+            }
+        }
+    };
+
+    private void showLeaveSchoolDialog() {
+        mLeaveSchoolDialog = new Dialog(this, R.style.DialogTheme);
+        mLeaveSchoolDialog.getWindow().setWindowAnimations(R.style.dialogWindowAnim);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.leave_school_dialog, null);
+        TextView confirmLeave = (TextView) contentView.findViewById(R.id.leave_school_dialog_confirm_leave_tv);
+        TextView cancelBtn = (TextView) contentView.findViewById(R.id.leave_school_dialog_cancel_tv);
+        confirmLeave.setOnClickListener(changePhotoListener);
+        cancelBtn.setOnClickListener(changePhotoListener);
+
+        mLeaveSchoolDialog.setContentView(contentView);
+        mLeaveSchoolDialog.setCanceledOnTouchOutside(true);
+        mLeaveSchoolDialog.show();
+
+        WindowManager.LayoutParams params = mLeaveSchoolDialog.getWindow()
+                .getAttributes();
+        params.gravity = Gravity.BOTTOM;
+        params.width = UtilTools.SCREEN_WIDTH;
+        mLeaveSchoolDialog.getWindow().setAttributes(params);
+    }
+
+
+    private void showModifyClassDialog() {
+        mModifyClassDialog = new Dialog(this, R.style.DialogTheme);
+        mModifyClassDialog.getWindow().setWindowAnimations(R.style.dialogWindowAnim);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.modify_class_dialog, null);
+        Button confirm = (Button) contentView.findViewById(R.id.modify_class_dialog_confirm_btn);
+        ListView classLv = (ListView) contentView.findViewById(R.id.modify_class_dialog_lv);
+        confirm.setOnClickListener(changePhotoListener);
+
+        final ModifyClassDialogLvAdapter mcda = new ModifyClassDialogLvAdapter(this,getTestData());
+        classLv.setAdapter(mcda);
+        classLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mcda.setSelectPosition(position);
+            }
+        });
+
+        mModifyClassDialog.setContentView(contentView);
+        mModifyClassDialog.setCanceledOnTouchOutside(true);
+        mModifyClassDialog.show();
+
+        WindowManager.LayoutParams params = mModifyClassDialog.getWindow().getAttributes();
+        params.gravity = Gravity.BOTTOM;
+        params.width = UtilTools.SCREEN_WIDTH;
+        mModifyClassDialog.getWindow().setAttributes(params);
+    }
+
+    private ArrayList<MakeClassAddPersonInfo> getTestData() {
+        ArrayList<MakeClassAddPersonInfo> data = new ArrayList<MakeClassAddPersonInfo>();
+        for (int i = 0 ; i < 3; i ++) {
+            MakeClassAddPersonInfo ma = new MakeClassAddPersonInfo();
+            if (i == 0) {
+                ma.setName("柠檬班");
+            }
+
+            if (i == 1) {
+                ma.setName("橘子班");
+            }
+
+            if (i == 2) {
+                ma.setName("苹果班");
+            }
+            ma.setId(String.valueOf(i + 1));
+            data.add(ma);
+        }
+        return data;
     }
 }
---
+++ app/src/main/java/com/jgkj/parentscycle/utils/DialogUtil.java
@@ -0,0 +1,15 @@
+package com.jgkj.parentscycle.utils;
+
+/**
+ * Created by chen on 16/8/5.
+ */
+public class DialogUtil {
+    public interface DialogClicked{
+        public void onDialogClicked(int position);
+    }
+
+    public static void showBottomCustomDialot(DialogClicked dialogClicked,int flag) {
+
+    }
+
+}
