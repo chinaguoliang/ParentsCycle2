a4ecb4c8f3412d85076e66a613257a62e1f32f03
chen <38033919@qq.com>
2016-08-20 04:33:53 +0800

    增加发布公告页面

---
+++ app/src/main/java/com/jgkj/parentscycle/activity/PublishAnnouncementActivity.java
@@ -0,0 +1,16 @@
+package com.jgkj.parentscycle.activity;
+
+import android.os.Bundle;
+
+import com.jgkj.parentscycle.R;
+
+/**
+ * Created by chen on 16/8/20.
+ */
+public class PublishAnnouncementActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.publish_announcement_activity);
+    }
+}
