c0f5c098be5566d9417a99ebfea9020289ecb544
chen <38033919@qq.com>
2016-09-11 00:22:20 +0800

    修改宝宝详情页面

--- app/src/main/java/com/jgkj/parentscycle/activity/BabyInfoActivity.java
+++ app/src/main/java/com/jgkj/parentscycle/activity/BabyInfoActivity.java
@@ -1,18 +1,26 @@
 package com.jgkj.parentscycle.activity;
 
+import android.app.DatePickerDialog;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jgkj.parentscycle.R;
 import com.jgkj.parentscycle.bean.TeacherInfoListInfo;
 import com.jgkj.parentscycle.user.UserInfo;
 import com.jgkj.parentscycle.widget.ListViewForScrollView;
+import com.jgkj.parentscycle.widget.SexSelectDialog;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,7 +29,7 @@ import butterknife.OnClick;
 /**
  * Created by chen on 16/8/30.
  */
-public class BabyInfoActivity extends BaseActivity implements View.OnClickListener{
+public class BabyInfoActivity extends BaseActivity implements View.OnClickListener,SexSelectDialog.SexSlectDialogFinish,DatePickerDialog.OnDateSetListener{
     @Bind(R.id.title_bar_layout_rel)
     View titleBg;
 
@@ -34,8 +42,87 @@ public class BabyInfoActivity extends BaseActivity implements View.OnClickListen
     @Bind(R.id.baby_document_right_title_tv)
     TextView rightTv;
 
-    @Bind(R.id.baby_info_activity_lv)
-    ListViewForScrollView contentLv;
+
+    @Bind(R.id.baby_info_activity_name_rel)
+    RelativeLayout nameRel;
+
+    @Bind(R.id.baby_info_activity_name_et)
+    EditText nameet;
+
+
+    @Bind(R.id.baby_info_activity_sex_rel)
+    RelativeLayout sexRel;
+
+    @Bind(R.id.baby_info_activity_sex_tv)
+    TextView sexTv;
+
+
+    @Bind(R.id.baby_info_activity_birthday_rel)
+    RelativeLayout birthdayRel;
+
+    @Bind(R.id.baby_info_activity_birthday_tv)
+    TextView birthdayTv;
+
+
+    @Bind(R.id.baby_info_activity_age_rel)
+    RelativeLayout ageRel;
+
+    @Bind(R.id.baby_info_activity_age_et)
+    EditText ageEt;
+
+
+    @Bind(R.id.baby_info_activity_to_school_time_rel)
+    RelativeLayout toSchoolTimeRel;
+
+    @Bind(R.id.baby_info_activity_to_school_time_tv)
+    TextView toSchoolTimeTv;
+
+
+    @Bind(R.id.baby_info_activity_farther_rel)
+    RelativeLayout fartherRel;
+
+    @Bind(R.id.baby_info_activity_farther_et)
+    EditText fartherEt;
+
+
+    @Bind(R.id.baby_info_activity_farther_phone_rel)
+    RelativeLayout fartherPhoneRel;
+
+    @Bind(R.id.baby_info_activity_farther_phone_et)
+    EditText fartherPhoneEt;
+
+
+    @Bind(R.id.baby_info_activity_monther_rel)
+    RelativeLayout montherRel;
+
+    @Bind(R.id.baby_info_activity_monther_et)
+    EditText montherEt;
+
+
+    @Bind(R.id.baby_info_activity_monther_phone_rel)
+    RelativeLayout montherPhoneRel;
+
+    @Bind(R.id.baby_info_activity_monther_phone_et)
+    EditText montherPhoneEt;
+
+    @Bind(R.id.baby_info_activity_belong_class_rel)
+    RelativeLayout belongClassRel;
+
+    @Bind(R.id.baby_info_activity_belong_class_tv)
+    TextView belongClassTv;
+
+
+    @Bind(R.id.baby_info_activity_join_class_rel)
+    RelativeLayout joinClassRel;
+
+    @Bind(R.id.baby_info_activity_transfer_class_rel)
+    RelativeLayout transferClassRel;
+
+    @Bind(R.id.baby_info_activity_leave_school_rel)
+    RelativeLayout leaveSchoolRel;
+
+    int selSex = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,28 +142,67 @@ public class BabyInfoActivity extends BaseActivity implements View.OnClickListen
 
     }
 
-    @OnClick({R.id.baby_document_activity_back_iv})
+    @OnClick({R.id.baby_document_activity_back_iv,
+              R.id.baby_info_activity_join_class_rel,
+              R.id.baby_info_activity_sex_rel,
+            R.id.baby_info_activity_birthday_rel,
+            R.id.baby_info_activity_transfer_class_rel,
+            R.id.baby_info_activity_leave_school_rel,
+            R.id.baby_info_activity_to_school_time_rel
+
+
+    })
     @Override
     public void onClick(View v) {
         if (v == backIv) {
             finish();
+        } else if (v == joinClassRel) {
+
+        } else if (v == sexRel) {
+            SexSelectDialog.showSexSelectDialog(BabyInfoActivity.this,BabyInfoActivity.this);
+        } else if (v == birthdayRel) {
+            showDateDialog(1);
+        } else if (v == transferClassRel) {
+
+        } else if (v == leaveSchoolRel) {
+
+        } else if (v == toSchoolTimeRel) {
+            showDateDialog(2);
         }
     }
 
-    private List<String> getContentData(TeacherInfoListInfo tii) {
-        ArrayList<String> data = new ArrayList<String>();
-        data.add("职务_" + tii.getAnalysis());
-        data.add("权限_" + UserInfo.getTitleByPermission(tii.getPermissions()));
-        data.add("昵称_" + tii.getNickname());
-        data.add("姓名_" + tii.getTeachername());
-        data.add("性别_" + UserInfo.getSexByServerData(tii.getTeachersex()));
-        data.add("民族_" + tii.getNationality());
-        data.add("出生日期_" + tii.getBirthdate());
-        data.add("手机号_" + tii.getPhone());
-        data.add("负责班级_" + tii.getClassid());
-        data.add("更改职位与权限_");
-        data.add("更改班级_");
-        data.add("离开幼儿园_");
-        return data;
+
+    private void showDateDialog(int flag) {
+        Calendar d = Calendar.getInstance(Locale.CHINA);
+        int year=d.get(Calendar.YEAR);
+        int month=d.get(Calendar.MONTH);
+        int day=d.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(this,android.R.style.Theme_Holo_Light_Dialog,this,year,month,day);
+        dpd.getDatePicker().setTag(flag);
+        dpd.getWindow().setBackgroundDrawable(new BitmapDrawable()); //设置为透明
+        dpd.show();
+    }
+
+    @Override
+    public void finishSlecct(int index) {
+        if (index == 1) {
+            sexTv.setText("男");
+            selSex = 1;
+        } else if (index == 0) {
+            sexTv.setText("女");
+            selSex = 0;
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        String date = year + "-" + (monthOfYear + 1) + "-" +dayOfMonth;
+        int flag = Integer.parseInt(view.getTag().toString());
+        if (flag == 1) {
+            birthdayTv.setText(date);
+        } else if (flag == 2) {
+            toSchoolTimeTv.setText(date);
+        }
     }
 }
