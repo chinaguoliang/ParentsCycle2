d46f4d4ee8edf373ee36fffd2701854ff3df33ec
chen <38033919@qq.com>
2016-11-07 00:13:41 +0800

    去除时段控制按钮

--- app/src/main/java/com/jgkj/parentscycle/activity/RegisterActivity.java
+++ app/src/main/java/com/jgkj/parentscycle/activity/RegisterActivity.java
@@ -4,11 +4,20 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chatuidemo.DemoApplication;
+import com.hyphenate.chatuidemo.DemoHelper;
+import com.hyphenate.chatuidemo.db.DemoDBManager;
+import com.hyphenate.chatuidemo.ui.*;
+import com.hyphenate.chatuidemo.ui.LoginActivity;
+import com.hyphenate.chatuidemo.ui.MainActivity;
 import com.jgkj.parentscycle.R;
 import com.jgkj.parentscycle.bean.GetVerifyNumInfo;
 import com.jgkj.parentscycle.bean.RegisterInfo;
@@ -21,6 +30,7 @@ import com.jgkj.parentscycle.net.NetListener;
 import com.jgkj.parentscycle.net.NetRequest;
 import com.jgkj.parentscycle.utils.LogUtil;
 import com.jgkj.parentscycle.utils.ToastUtil;
+import com.jgkj.parentscycle.utils.UtilTools;
 import com.jgkj.parentscycle.widget.ButtonTimeCount;
 
 import java.util.HashMap;
@@ -69,7 +79,11 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
             if (!hadShow) {
                 return;
             }
-            requestRegister();
+
+            String phone = phoneNumEt.getText().toString().trim();
+            String password = passwordEt.getText().toString().trim();
+            logout(phone,password);
+
         } else if (v == getVerifyPhoneNumTv) {
             boolean hadShow = showProgressDialog();
             if (!hadShow) {
@@ -137,5 +151,101 @@ public class RegisterActivity extends BaseActivity implements View.OnClickListen
     @Override
     public void uploadImgFinished(Bitmap bitmap,String uploadedKey) {
 
+    }
+
+
+    private void registerChatAndLogin(final String userName,final String userNamePwd) {
+        showProgressDialog();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final String userNameMd5Str = UtilTools.getMD5(userName);
+                    EMClient.getInstance().createAccount(userNameMd5Str, userNamePwd);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            // save current user
+                            DemoHelper.getInstance().setCurrentUserName(userNameMd5Str);
+                            loginChat(userName,userNamePwd);
+                        }
+                    });
+                } catch (Exception e) {
+                    hideProgressDialog();
+                }
+            }
+        }).start();
+    }
+
+
+    private void loginChat(String userName,String userPwd) {
+        final String userNameMd5Str = UtilTools.getMD5(userName);
+        DemoDBManager.getInstance().closeDB();
+
+        // reset current user name before login
+        DemoHelper.getInstance().setCurrentUserName(userNameMd5Str);
+
+        final long start = System.currentTimeMillis();
+        // call login method
+        LogUtil.d("result","----->userName:" + EMClient.getInstance().getCurrentUser() + "|" + userNameMd5Str);
+        EMClient.getInstance().login(userNameMd5Str, userPwd, new EMCallBack() {
+
+            @Override
+            public void onSuccess() {
+
+                // ** manually load all local groups and conversation
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+
+                // update current user's display name for APNs
+                boolean updatenick = EMClient.getInstance().updateCurrentUserNick(
+                        DemoApplication.currentUserNick.trim());
+                if (!updatenick) {
+                    Log.e("LoginActivity", "update current user nick fail");
+                }
+
+                // get user's info (this should be get from App's server or 3rd party service)
+                DemoHelper.getInstance().getUserProfileManager().asyncGetCurrentUserInfo();
+                hideProgressDialog();
+                requestRegister();
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+                Log.d(TAG, "login: onProgress");
+            }
+
+            @Override
+            public void onError(final int code, final String message) {
+                Log.d(TAG, "login: onError: " + code);
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        hideProgressDialog();
+                        Toast.makeText(getApplicationContext(), getString(com.hyphenate.chatuidemo.R.string.Login_failed) + message,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+
+    void logout(final String phone,final String password) {
+        DemoHelper.getInstance().logout(false,new EMCallBack() {
+
+            @Override
+            public void onSuccess() {
+                registerChatAndLogin(phone,password);
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/jgkj/parentscycle/utils/UtilTools.java
+++ app/src/main/java/com/jgkj/parentscycle/utils/UtilTools.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -14,6 +15,8 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -948,4 +951,39 @@ public class UtilTools {
 //
 //		return badgeCount;
 //	}
+
+
+	//获得MD5码
+	public static String getMD5(String info)
+	{
+		try
+		{
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update(info.getBytes("UTF-8"));
+			byte[] encryption = md5.digest();
+
+			StringBuffer strBuf = new StringBuffer();
+			for (int i = 0; i < encryption.length; i++)
+			{
+				if (Integer.toHexString(0xff & encryption[i]).length() == 1)
+				{
+					strBuf.append("0").append(Integer.toHexString(0xff & encryption[i]));
+				}
+				else
+				{
+					strBuf.append(Integer.toHexString(0xff & encryption[i]));
+				}
+			}
+
+			return strBuf.toString();
+		}
+		catch (NoSuchAlgorithmException e)
+		{
+			return "";
+		}
+		catch (UnsupportedEncodingException e)
+		{
+			return "";
+		}
+	}
 }
