e5064c56159ea69056ff3e044cc0a83257f159fe
chen <38033919@qq.com>
2016-11-20 14:57:49 +0800

    增加成长记录列表

---
+++ app/src/main/java/com/jgkj/parentscycle/bean/GrowthRecordInfo.java
@@ -0,0 +1,18 @@
+package com.jgkj.parentscycle.bean;
+
+import java.util.List;
+
+/**
+ * Created by chen on 16/11/20.
+ */
+public class GrowthRecordInfo {
+    List <GrowthRecordItemInfo> obj;
+
+    public List<GrowthRecordItemInfo> getObj() {
+        return obj;
+    }
+
+    public void setObj(List<GrowthRecordItemInfo> obj) {
+        this.obj = obj;
+    }
+}
---
+++ app/src/main/java/com/jgkj/parentscycle/bean/GrowthRecordItemInfo.java
@@ -0,0 +1,115 @@
+package com.jgkj.parentscycle.bean;
+
+/**
+ * Created by chen on 16/11/20.
+ */
+public class GrowthRecordItemInfo {
+    String attentionnum = "";
+    String babyid = "";
+    String commentnum = "";
+    String growthid = "";
+    String page = "";
+    String publisher = "";
+    String publisherdate = "";
+    String publisherimge = "";
+    String publishername = "";
+    String publishertext = "";
+    String rows = "";
+    String thumbnum = "";
+
+    public String getPublisherdate() {
+        return publisherdate;
+    }
+
+    public void setPublisherdate(String publisherdate) {
+        this.publisherdate = publisherdate;
+    }
+
+    public String getAttentionnum() {
+        return attentionnum;
+    }
+
+    public void setAttentionnum(String attentionnum) {
+        this.attentionnum = attentionnum;
+    }
+
+    public String getBabyid() {
+        return babyid;
+    }
+
+    public void setBabyid(String babyid) {
+        this.babyid = babyid;
+    }
+
+    public String getCommentnum() {
+        return commentnum;
+    }
+
+    public void setCommentnum(String commentnum) {
+        this.commentnum = commentnum;
+    }
+
+    public String getGrowthid() {
+        return growthid;
+    }
+
+    public void setGrowthid(String growthid) {
+        this.growthid = growthid;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisherimge() {
+        return publisherimge;
+    }
+
+    public void setPublisherimge(String publisherimge) {
+        this.publisherimge = publisherimge;
+    }
+
+    public String getPublishername() {
+        return publishername;
+    }
+
+    public void setPublishername(String publishername) {
+        this.publishername = publishername;
+    }
+
+    public String getPublishertext() {
+        return publishertext;
+    }
+
+    public void setPublishertext(String publishertext) {
+        this.publishertext = publishertext;
+    }
+
+    public String getRows() {
+        return rows;
+    }
+
+    public void setRows(String rows) {
+        this.rows = rows;
+    }
+
+    public String getThumbnum() {
+        return thumbnum;
+    }
+
+    public void setThumbnum(String thumbnum) {
+        this.thumbnum = thumbnum;
+    }
+}
