ffaaf6816d1500b1923b2018bb9e616dcea732e9
chen <38033919@qq.com>
2016-11-29 03:34:07 +0800

    修改父母圈发贴

--- app/src/main/java/com/jgkj/parentscycle/activity/ParentsCyclePersonalPageActivity.java
+++ app/src/main/java/com/jgkj/parentscycle/activity/ParentsCyclePersonalPageActivity.java
@@ -11,13 +11,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jgkj.parentscycle.R;
 import com.jgkj.parentscycle.adapter.ParentsCycleAdapter;
 import com.jgkj.parentscycle.bean.ParentsCycleInfo;
+import com.jgkj.parentscycle.bean.ParentsCycleListInfo;
+import com.jgkj.parentscycle.bean.ParentsCyclePostsListItem;
 import com.jgkj.parentscycle.customview.HeaderGridView;
+import com.jgkj.parentscycle.global.BgGlobal;
+import com.jgkj.parentscycle.json.ParentsCyclePostsListItemPaser;
+import com.jgkj.parentscycle.net.NetBeanSuper;
+import com.jgkj.parentscycle.net.NetListener;
+import com.jgkj.parentscycle.net.NetRequest;
+import com.jgkj.parentscycle.utils.ToastUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,7 +36,7 @@ import butterknife.OnClick;
 /**
  * Created by chen on 16/8/27.
  */
-public class ParentsCyclePersonalPageActivity extends BaseActivity implements View.OnClickListener {
+public class ParentsCyclePersonalPageActivity extends BaseActivity implements View.OnClickListener,NetListener {
 
     @Bind(R.id.title_bar_layout_rel)
     RelativeLayout mWrapTitleRel;
@@ -60,7 +70,7 @@ public class ParentsCyclePersonalPageActivity extends BaseActivity implements Vi
 
 
     private void initViews() {
-        mWrapTitleRel.setBackgroundColor(this.getResources().getColor(R.color.white));
+        mWrapTitleRel.setBackgroundColor(this.getResources().getColor(R.color.transparent));
         rightTitleTv.setVisibility(View.GONE);
         titleTv.setText("");
         titleTv.setTextColor(this.getResources().getColor(R.color.black));
@@ -69,54 +79,21 @@ public class ParentsCyclePersonalPageActivity extends BaseActivity implements Vi
         View headerView = inflater.inflate(R.layout.parents_cycle_personalpage_activity_listview_header_view,null);
         mContentLv.addHeaderView(headerView);
 
-//        ArrayList<ParentsCycleInfo> dataList = getTestData(0);
-//        mParentsCycleAdapter = new ParentsCycleAdapter(this,dataList);
-//        mContentLv.setAdapter(mParentsCycleAdapter);
-//        mContentLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                if (position != 0) {
-//                    startActivity(new Intent(view.getContext(),ParentsCycleDetailActivity.class));
-//                }
-//            }
-//        });
-//
-//        perfectInfoIv = (ImageView)headerView.findViewById(R.id.parents_cycle_personalpage_activity_perfect_info_iv);
-//        perfectInfoIv.setOnClickListener(this);
+
+
+        perfectInfoIv = (ImageView)headerView.findViewById(R.id.parents_cycle_personalpage_activity_perfect_info_iv);
+        perfectInfoIv.setOnClickListener(this);
+        requestParentsCycleList();
     }
 
-    private ArrayList<ParentsCycleInfo> getTestData(int flag) {
-        ArrayList<ParentsCycleInfo> dataList = new ArrayList<ParentsCycleInfo>();
-        for(int i = 0 ; i < 30 ; i++) {
-            if (i % 2 == 0) {
-                ParentsCycleInfo pci = new ParentsCycleInfo();
-                pci.setName("小明");
-                pci.setTime("1小时前");
-                pci.setContent("这个幼儿园很好");
-                if (flag == 0) {
-                    pci.setImgRes(R.mipmap.baby_school_inside);
-                } else if (flag == 1) {
-                    pci.setImgRes(R.mipmap.school2);
-                } else if (flag == 2) {
-                    pci.setImgRes(R.mipmap.school3);
-                }
-                dataList.add(pci);
-            } else {
-                ParentsCycleInfo pci = new ParentsCycleInfo();
-                pci.setName("小惠");
-                pci.setTime("2小时前");
-                pci.setContent("市内最好的幼儿园");
-                dataList.add(pci);
-                if (flag == 0) {
-                    pci.setImgRes(R.mipmap.baby_school_inside);
-                } else if (flag == 1) {
-                    pci.setImgRes(R.mipmap.school2);
-                } else if (flag == 2) {
-                    pci.setImgRes(R.mipmap.school3);
-                }
-            }
-        }
-        return dataList;
+    //食谱发布
+    public void requestParentsCycleList() {
+        HashMap<String, String> requestData = new HashMap<String, String>();
+        requestData.put("rows", "30");
+        requestData.put("page","1");
+        ParentsCyclePostsListItemPaser lp = new ParentsCyclePostsListItemPaser();
+        NetRequest.getInstance().request(mQueue, this,
+                BgGlobal.PARENTS_CYCLE_POSTS_LIST, requestData, lp);
     }
 
     @OnClick({R.id.baby_document_activity_back_iv,R.id.parents_cycle_personalpage_activity_send_msg_btn})
@@ -134,5 +111,35 @@ public class ParentsCyclePersonalPageActivity extends BaseActivity implements Vi
     @Override
     public void uploadImgFinished(Bitmap bitmap, String uploadedKey) {
 
+    }
+
+    @Override
+    public void requestResponse(Object obj) {
+        hideProgressDialog();
+        NetBeanSuper nbs = (NetBeanSuper)obj;
+        if (nbs.obj instanceof ParentsCycleListInfo) {
+            if (nbs.isSuccess()) {
+                ParentsCycleListInfo tii = (ParentsCycleListInfo)nbs.obj;
+                mParentsCycleAdapter = new ParentsCycleAdapter(this,tii.getObj());
+                mContentLv.setAdapter(mParentsCycleAdapter);
+                mContentLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        int targetPos = position - 2;
+                        if (targetPos < 0) {
+                            targetPos = 0;
+                        }
+                        ParentsCyclePostsListItem pcpli = (ParentsCyclePostsListItem)mParentsCycleAdapter.getItem(targetPos);
+                        Intent intent = new Intent(view.getContext(),ParentsCycleDetailActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putSerializable("parentsinfo",pcpli);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
+                    }
+                });
+            } else {
+                ToastUtil.showToast(this,nbs.getMsg(), Toast.LENGTH_SHORT);
+            }
+        }
     }
 }
