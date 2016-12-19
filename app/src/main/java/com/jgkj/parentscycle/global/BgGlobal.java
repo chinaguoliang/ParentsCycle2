b4570a177f3838b5c007a7505587a68a03a31738
guoliang.chen@21chinamall.com <guoliang.chen@21chinamall.com>
2016-12-19 17:45:29 +0800

    去除多余的URL

--- app/src/main/java/com/jgkj/parentscycle/global/BgGlobal.java
+++ app/src/main/java/com/jgkj/parentscycle/global/BgGlobal.java
@@ -76,8 +76,6 @@ public class BgGlobal {
     public static final String PARENTS_INFO_AUDIT = "parentinfo/updateParentinfo"; //家长信息审核验证
 
     public static final String TEACHER_INFO_MODIFY_OR_EXAMINE = "teacherinfo/updateTeacherinfo";//教师信息修改或者审核
-    public static final String PARENTS_INFO_LIST = "parentinfo/parentinfoList";//家长信息列表
-    public static final String TEACHER_EXAMINE = "parentinfo/updateParentinfo";//家长信息审核验证
     public static final String BABY_JOIN_CLASS = "classBaby/save"; //宝宝加入班级
     public static final String SCHOOL_LEADER_EXAMINE_TEACHER_JOIN_SHCOOL = "schoolTeacher/schoolTeacherUpdate";//园长审核老师加入学校
     public static final String TEACHER_EXAMINE_STUDENT_JOIN_CLASS = "classBaby/update";//老师审核学生加入班级
